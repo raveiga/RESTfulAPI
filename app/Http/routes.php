80a22428171b487cb5f61f5c344f922966a9cc0f
Rafa Veiga <raveiga32@gmail.com>
2015-04-04 00:18:32 +0200

    Agregando versionado a la API
    - Se agrega un grupo a las rutas poniendo el prefijo de la versión.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,9 +13,14 @@
 // Las rutas edit se utilizan para mostrar un formulario de edición.
 // Las rutas de create se utilizan para mostrar un formulario de creación de datos.
 
-Route::resource('vehiculos','VehiculoController',['only'=>['index','show']]);
-Route::resource('fabricantes','FabricanteController',['except'=>['edit','create']]);
-Route::resource('fabricantes.vehiculos','FabricanteVehiculoController',['except'=>['show','edit','create']]);
+// Añadimos un prefijo a la api: /api/v1.1/...
+Route::group(array('prefix'=>'api/v1.1',function()
+{
+	Route::resource('vehiculos','VehiculoController',['only'=>['index','show']]);
+	Route::resource('fabricantes','FabricanteController',['except'=>['edit','create']]);
+	Route::resource('fabricantes.vehiculos','FabricanteVehiculoController',['except'=>['show','edit','create']]);
+}));
+
 
 // Mensaje para rutas inexistentes con una expresión regular.
 // Patrón para la definición de ruta.
